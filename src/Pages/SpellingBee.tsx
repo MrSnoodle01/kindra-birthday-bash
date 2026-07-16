1e5ba870841115cbd1c925f3c393db00c0fd1887
MrSnoodle01 <richardsonjosh03@gmail.com>
2026-07-15 19:43:45 -0700

    changed words to be more accurately rated

--- src/Pages/SpellingBee.tsx
+++ src/Pages/SpellingBee.tsx
@@ -15,10 +15,38 @@ interface Word {
     example: string
 }
 
+const sleepyImages = [
+    "/sleepy/20250419_185238.jpg",
+    "/sleepy/IMG_1106.jpg",
+    "/sleepy/IMG_1310.jpg",
+    "/sleepy/IMG_1524.jpg",
+    "/sleepy/IMG_2660.jpg",
+    "/sleepy/IMG_4159.jpg",
+    "/sleepy/IMG_4302.jpg",
+    "/sleepy/IMG_5133.jpg",
+    "/sleepy/IMG_6448.jpg",
+    "/sleepy/IMG_6581.jpg",
+    "/sleepy/IMG_8277.jpg",
+    "/sleepy/IMG_8330.jpg",
+    "/sleepy/IMG_8445.jpg",
+    "/sleepy/IMG_8446.jpg",
+    "/sleepy/IMG_8563.jpg",
+    "/sleepy/IMG_9998.jpg",
+]
+
 export default function SpellingBee() {
     const [currentWord, setCurrentWord] = useState<Word | null>(null);
 
     const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+
+    const [imageSettings] = useState(() =>
+        sleepyImages.map(() => ({
+            top: Math.random() * 90,
+            left: Math.random() * 90,
+            duration: 8 + Math.random() * 10,
+            delay: Math.random() * 5,
+        }))
+    );
 
     useEffect(() => {
         function loadVoices() {
@@ -75,6 +103,21 @@ export default function SpellingBee() {
 
     return (
         <div className="page">
+
+            <div className="backgroundImages">
+                {sleepyImages.map((image, index) => (
+                    <img
+                        key={image}
+                        className="floatingImage"
+                        src={image}
+                        style={{
+                            top: `${imageSettings[index].top}%`,
+                            animationDuration: `${imageSettings[index].duration}s`,
+                            animationDelay: `${imageSettings[index].delay}s`,
+                        }}
+                    />
+                ))}
+            </div>
 
             <div className="wordContainer">
                 <button onClick={() => currentWord && speakWord(currentWord.word)}>
